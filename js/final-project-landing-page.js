e28696e9feadcf28514a5ad035ddb4d2043d9510
Jianna <saraqkr@gmail.com>
2019-09-04 15:14:53 -0400

    Create final-project-landing-page.js

---
+++ js/final-project-landing-page.js
@@ -0,0 +1,40 @@
+// 1 function specific to the landing page
+
+/* 1. make the background image automatically fade to next index item in the array every 8s */
+
+function onReady() {
+
+    const imagePaths = [
+        'background-images/1.jpg',
+        'background-images/2.jpg',
+        'background-images/3.jpg',
+        'background-images/4.jpg'
+    ];
+    const titles = [
+        'The Lasso is Cut with a Strike of a Match (2019)', 
+        'Rubber Puppy Interrogation (2019)',
+        'Silly Monkey Who Lost His Head and a Leg and Fell From a Tree and Died Before He Could Count to Three (2018)',
+        'In a Perfect World, Every Cow Would Be Exactly Identical and Have Flawless Coats of Fur With No Marks or Blemishes (2018)']
+    ;
+    var slideIndex = 0;
+
+    function showSlides() {
+
+        const currentImagePath = imagePaths[slideIndex];
+        const currentTitle = titles[slideIndex];
+
+        $('body').css('background-image', 'url("' + currentImagePath + '")'); 
+        $('.title').html(currentTitle);
+
+        slideIndex++;
+        if (slideIndex >= imagePaths.length || slideIndex >= titles.length) {
+            slideIndex = 0;
+        }
+
+        setTimeout(showSlides, 8000);
+    }
+
+    $('body').ready(showSlides);
+}
+
+$('document').ready(onReady);
