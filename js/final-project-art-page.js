52250f9a84907e892d9525f4d5536923da493bb2
Jianna <saraqkr@gmail.com>
2019-09-16 22:20:46 -0400

    Update final-project-art-page.js

--- js/final-project-art-page.js
+++ js/final-project-art-page.js
@@ -61,25 +61,25 @@ function onReady() {
 
     // add images by loop without flooding html
     const imageDirJuicy = 'Zack Thesis Portfolio/Images/Davis_Zachary_04_Juicy Fruit Falling from Yonder Blue_mixed media_8x4x3ft_Sculpture_2019/'
-    const imagePathsJuicy = ['04 Center.jpg', '04 Detail 1.jpg', '04 Detail 2.jpg', '04 Detail 3.jpg', 'P1070110.JPG', 'P1070122 2.JPG', 'P1070197 3.jpg']
+    const imagePathsJuicy = ['04 Center.jpg', '04 Detail 1.jpg', 'P1070197 3.jpg', '04 Detail 2.jpg', 'P1070122 2.JPG', '04 Detail 3.jpg', 'P1070110.JPG']
 
     const imageDirRubber = 'Zack Thesis Portfolio/Images/Davis_Zachary_05_Rubber Puppy Interrogation_mixed media_6x6x4ft_sculpture_2019/'
-    const imagePathsRubber = ['05 Center.JPG', '05 Detail.JPG', '05 Right.JPG']
+    const imagePathsRubber = ['05 Center.JPG', '05 Right.JPG', '05 Detail.JPG']
     
     const imageDirLasso = 'Zack Thesis Portfolio/Images/Davis_Zachary_06_The Lasso is Cut With the Strike of a Match_mixed media_1.5x4x4ft_Sculpture_2019/'
-    const imagePathsLasso = ['06 Center.JPG', '06 Detail 1.JPG', '06 Detail 2.JPG', '06 Detail 3.JPG', '06 Right.JPG']
+    const imagePathsLasso = ['06 Right.JPG', '06 Center.JPG', '06 Detail 1.JPG', '06 Detail 3.JPG', '06 Detail 2.JPG']
 
     const imageDirUntitled = 'Zack Thesis Portfolio/Images/Davis Zachary 07/'
-    const imagePathsUntitled = ['15.JPG', 'P1260691.JPG', 'P1260706.JPG', 'P1280132.JPG', 'P1280134.JPG', 'P1280137.JPG', 'P1280138.JPG', 'P1280158.JPG', 'Screen Shot 2018-11-25 at 8.59.42 PM.png', 'Screen Shot 2018-12-17 at 5.13.24 PM.png', 'Screen Shot 2018-12-17 at 5.14.43 PM.png']
+    const imagePathsUntitled = ['P1260691.JPG', 'P1260706.JPG', '15.JPG', 'P1280158.JPG', 'P1280132.JPG', 'P1280134.JPG', 'P1280137.JPG', 'P1280138.JPG', 'Screen Shot 2018-11-25 at 8.59.42 PM.png', 'Screen Shot 2018-12-17 at 5.13.24 PM.png', 'Screen Shot 2018-12-17 at 5.14.43 PM.png']
 
     const imageDirSilly = 'Zack Thesis Portfolio/Images/Davis_Zachary_01_Silly Monkey Who Lost His head and a Leg and Fell From a Tree and Died Before He Could Count to Three_mixed media_2x1x5ft_sculpture_2018/'
-    const imagePathsSilly = ['01 Left.JPG', '01 Right.JPG', '01 Detail.JPG']
+    const imagePathsSilly = ['01 Right.JPG', '01 Detail.JPG', '01 Left.JPG']
 
     const imageDirAftermath = 'Zack Thesis Portfolio/Images/Davis_Zachary_02_Aftermathmatic Reverse Dissection_mixed media_3x4x6ft ft_Sculpture_2018./'
-    const imagePathsAftermath = ['02 Right.JPG', '02 Left.JPG', '02 Center.JPG', '_1030963.JPG']
+    const imagePathsAftermath = ['02 Center.JPG', '02 Left.JPG', '02 Right.JPG', '_1030963.JPG']
 
     const imageDirGod = 'Zack Thesis Portfolio/Images/Davis_Zachary_03_God’s in Heaven; Feeding Us the Precious Nectar Straight from the Divine Teat; All’s Right with the World_Mixed Media_6x7x3ft_sculpture_2018/'
-    const imagePathsGod = ['03 Center.JPG', '03 Detail 1.JPG', '03 Detail 2.JPG', 'P1320089.JPG', 'Screen Shot 2019-05-13 at 4.48.24 PM.png', 'tumblr_pjwiugPfc11r23pupo1_500.jpg']
+    const imagePathsGod = ['03 Center.JPG', '03 Detail 2.JPG', '03 Detail 1.JPG', 'Screen Shot 2019-05-13 at 4.48.24 PM.png', 'P1320089.JPG', 'tumblr_pjwiugPfc11r23pupo1_500.jpg']
 
     var i
 
