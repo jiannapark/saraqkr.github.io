0075f39fa1b0674d8a9790afd587221bffb42d31
Jianna <saraqkr@gmail.com>
2019-09-16 22:22:01 -0400

    Update final-project-contact.js

--- js/final-project-contact.js
+++ js/final-project-contact.js
@@ -1,5 +1,3 @@
-// # functions specific to contact page
-
 /* 
  * two functions (characterCounter and formSelect) are from materializecss.com: https://materializecss.com/text-inputs.html and https://materializecss.com/select.html
 */
@@ -10,3 +8,7 @@ function onReady() {
 }
 
 $('document').ready(onReady);
+
+
+// see "registration.js" file. (can you send the query parameters to my email?) 
+// file:///Users/jianna/Desktop/Study/Front-End%20Web%20Development%2010-week%20course/FEWD/GitHub/saraqkr.github.io/final-project-contact-success.html?Click+Me%21=Submit
