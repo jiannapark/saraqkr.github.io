2bfc444d5da1326e48ad5b80665b17228fac1faa
Jianna <saraqkr@gmail.com>
2019-09-22 01:11:48 -0400

    Update final-project-contact.js

--- js/final-project-contact.js
+++ js/final-project-contact.js
@@ -1,14 +1,15 @@
+// 3 function specific to the contact page
+
 /* 
- * two functions (characterCounter and formSelect) are from materializecss.com: https://materializecss.com/text-inputs.html and https://materializecss.com/select.html
+ * the first two functions (characterCounter and formSelect) are from materializecss.com: https://materializecss.com/text-inputs.html and https://materializecss.com/select.html
 */
 
 function onReady() {
+    /* 1. count number of characters in message */
     $('textarea#textarea1').characterCounter();
+    /* 2. select a title from dropdown menu */
     $('select').formSelect();
+    /* 3. pull out user input and send an email to Zack -- more work on this later */
 }
 
 $('document').ready(onReady);
-
-
-// see "registration.js" file. (can you send the query parameters to my email?) 
-// file:///Users/jianna/Desktop/Study/Front-End%20Web%20Development%2010-week%20course/FEWD/GitHub/saraqkr.github.io/final-project-contact-success.html?Click+Me%21=Submit
